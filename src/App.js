5fee7518f14385c04b9f85fb86e74165e33bb915
[HobumPark] <[gloryofalive@naver.com]>
2023-01-06 15:33:07 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const getData=async()=>{
+    //axios.defaults.baseURL = 'http://mytest.cafe24app.com';
+    axios.defaults.headers.get['Content-Type'] ='application/json;charset=utf-8';
+    axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+    axios.defaults.headers.get['widthCredentials'] = true
+    const result=await axios.get('/board')
+    console.log(result)
+  }
+
+  const getData2=async()=>{
+    const result=await axios.get('/board/2')
+    console.log(result)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={getData}>요청</button>
+        <button onClick={getData2}>요청</button>
     </div>
   );
 }
